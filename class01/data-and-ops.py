78662a27afe1a5b46928d20e93e753d904b7c57c
Persistent-A <apurva.mili@gmail.com>
2022-02-17 18:57:25 -0500

    Initial commit

---
+++ class01/data-and-ops.py
@@ -0,0 +1,22 @@
+# This is a comment which means it won't be read python.
+
+# Data Types
+5 # Int
+5.4 # Float
+'Hello' # String
+True  # Boolean
+
+# Operations
+# Math operators
+5+4
+5-4
+5*4
+5/4
+
+# Variable
+
+myVar = int(input("Enter a number: "))
+
+print(myVar + 5)
+
+#
